9a3219cf9673039f978d4222af5cc1cb26626499
TLance110 <deutscht7295@student.faytechcc.edu>
2025-02-23 19:15:26 -0500

    Add files via upload

---
+++ P1LAB1_DeutschTrevor.py
@@ -0,0 +1,12 @@
+#Trevor Deutsch
+#2/23/25
+#P1LAB1
+#Basic introduction program to learn user input and display outputs
+
+
+
+print('Please enter your first name:', end=' ')
+Ufirst = input()
+print('Please enter your last name:', end=' ')
+Ulast = input()
+print('Thank you. Welcome', Ufirst, Ulast, 'to CTI-110')
